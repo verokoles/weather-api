449ac6b62eadadcf6245f63c349b88c85f6ef076
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-03-20 01:59:41 -0500

    readme

--- README.md
+++ README.md
@@ -35,6 +35,28 @@ This Weather Dashboard displays the current and future weather and conditions fo
   
 <a name="steps"></a>
  ## 4. Steps:ladder:
+  To start, HTML is build with Bulma styuling to create box classes with widths to fit under the header title, WEATHER DASHBOARD. <br>
+  
+  ![Beginning page of Weather API dashboard](assets/dashboard-start.png)
+
+  Then, once all the boxes and columns line up next to eachother, the JavaScript is created with variables set. <br>
+  ![HTML with BULMA styling](assets/HTML-bulma.png)
+
+  The variables then can be used to call for coordinates with the GEO weather API (first link showed). <br>
+  ![JS variables](assets/JS-variables.png)
+
+  The second call from the One Call API then generates data like weather, UVI, wind speed, temperates, etc. <br>
+  ![API calls to fetch data for coordinates, then to display weather data and future forecast](assets/API-calls.png)
+
+  Once a city name is typed in the search bar, the city name, date, weather icon, and weather data pulled from the APIs <br>
+   display in the box to the right of Search City. Below the searched city then shows the next five days of forecast <br>
+   for that same city. If a city is spelled wrong, or nothing is typed, the user gets an error message. <br>
+   The recent cities that have been searched for will list under the text box for 'Search' to show the last five cities and their weather data.<br>
+ Using console.log in the Devtools was a major part in creating this app. It's a great way to check functionality of code and if data is even <br>
+ coming through, instead of just coding blindly. 
+ The UV index color coordinating comes from a scale that ranges 0-10 and in rare cases reaches over 11. <Br>
+  0-2 is low danger = green; 3-5 is moderate = yellow; 6-7 is high = orange; 8-10 is very high = red; higher than 10 is extreme = purple.<br>
+  A simple app like this can be a big help for a traveler searching for their destined city to see how to pack and plan accordingly.
 
  <a name="help"></a>
   ## 5. Helpful Links:link:
--- assets/script.js
+++ assets/script.js
@@ -29,6 +29,12 @@ humidityEl.setAttribute('card', 'card-text');
 
 var apiKey = "a7bb95be4838dc9c0a3fe02492f27fb9";
 
+// btnSearchCity.onclick = function (event) {
+//     console.log("Hello World!!!");
+//     event.preventDefault();
+
+//     formSubmitHandler();
+// };
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -38,73 +44,121 @@ var formSubmitHandler = function (event) {
     if (city) {
         getCoordinates(city);
 
-        // //clear old 
-        // cityContainerEl.textContent = "";
-        // cityInputEl.value = "";
     } else {
         alert("Please enter a valid city name");
     }
 };
 
+// using AJAX like jquery (asynchronous JS) to pull data and display
+function getCoordinates(city) {
+    $.ajax({
+        type: "GET",
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey,
+        async: true,
+        dataType: "json",
+        success: function (json) {
+            console.log(json);
+            console.log("First Data Call: ", data);
+            getCoordinates.json = json;
+            getWeather(json.coord);
+            getForecast(json.coord);
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+};
 
-    function getCoordinates(city) {
-        $.ajax({
-            type: "GET",
-            url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey,
-            async: true,
-            dataType: "json",
-            success: function (json) {
-                console.log(json);
-                getCoordinates.json = json;
-                getWeather(json.coord);
-                getForecast(json.coord);
-            },
-            error: function (err) {
-                console.log(err);
-            }
-        });
-    };
+function getWeather(coord) {
+    $.ajax({
+        type: "GET",
+        url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + coord.lat + "&lon=" + coord.lon + "&units=imperial&appid=" + apiKey,
+        async: true,
+        dataType: "json",
+        success: function (json) {
+            console.log(json);
+            console.log("Second Data Call: ", data);
+            getWeather.json = json;
+            displayWeather(json);
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+};
 
-    function getWeather(coord) {
-        $.ajax({
-            type: "GET",
-            url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + coord.lat + "&lon=" + coord.lon + "&units=imperial&appid=" + apiKey,
-            async: true,
-            dataType: "json",
-            success: function (json) {
-                console.log(json);
-                getWeather.json = json;
-                displayWeather(json);
-            },
-            error: function (err) {
-                console.log(err);
-            }
-        });
-    };
+function getForecast(coord) {
+    console.log(coord)
+    $.ajax({
+        type: "GET",
+        url: "https://api.openweathermap.org/data/2.5/forecast?lat=" + coord.lat + "&lon=" + coord.lon + "&units=imperial&appid=" + apiKey,
+        async: true,
+        dataType: "json",
+        success: function (json) {
+            console.log(json);
+            getForecast.json = json;
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+};
+function displayWeather(json) {
+    var displayCity = citySearchTerm.value.trim();
+    displayCity.textContent = "Displaying Current Weather For: " + displayCity;
+};
 
-    function getForecast(coord) {
-        console.log(coord)
-        $.ajax({
-            type: "GET",
-            url: "https://api.openweathermap.org/data/2.5/forecast?lat=" + coord.lat + "&lon=" + coord.lon + "&units=imperial&appid=" + apiKey,
-            async: true,
-            dataType: "json",
-            success: function (json) {
-                console.log(json);
-                getForecast.json = json;
-            },
-            error: function (err) {
-                console.log(err);
-            }
-        });
-    };
-    function displayWeather(json) {
-    };
-    function showRecentSearches(city) {};
-    //execute upon form submission browser event
-    
+function showRecentSearches(city) {
+    var displayCity = citySearchTerm.value.trim();
+    var btnSearchCity = document.createElement("button");
+    btnSearchCity.classList = "button is-fullwidth is-info";
+    btnSearchCity.textContent = displayCity;
+
+    recentSearchEl.appendChild();
+
+};
+//execute upon form submission browser event
+//getting current date and weather icon
+function showCurrentWeather(currentWeather, timezone) {
+    console.log(currentWeather);
+    console.log(currentWeather.weather[0].icon);
+    var iconcode = currentWeather.weather[0].icon;
+    var iconUrl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    console.log(iconUrl);
+}
+var moment = require('moment');
+//load moment.js library
+let now = moment();
+console.log(now.format());
+
+// set variables for the next five day forecast (date)
+var currentDate = moment();
+var displayCurrentDate = document.getElementById('current-date');
+displayCurrentDate.innerHTML = currentDate.format('MM-DD-YYYY');
+
+var displayDay1 = document.getElementById('day-1-date');
+displayDay1.innerHTML = currentDate.add(1, 'days').format('MM-DD-YYYY');
+var displayDay2 = document.getElementById('day-2-date');
+displayDay2.innerHTML = currentDate.add(2, 'days').format('MM-DD-YYYY');
+var displayDay3 = document.getElementById('day-3-date');
+displayDay3.innerHTML = currentDate.add(3, 'days').format('MM-DD-YYYY');
+var displayDay4 = document.getElementById('day-4-date');
+displayDay4.innerHTML = currentDate.add(4, 'days').format('MM-DD-YYYY');
+var displayDay5 = document.getElementById('day-5-date');
+displayDay5.innerHTML = currentDate.add(5, 'days').format('MM-DD-YYYY');
+console.log(days);
+// get temp, humidity, wind speed, and uvi for next five days
+
+//uv index should show oclor code
+// 0-2 is low danger = green
+// 3-5 is moderate = yellow
+// 6-7 is high = orange
+// 8-10 is very high = red
+// higher than 10 is extreme = purple
 
 
 
-//runs formsubmithandler when soeone click search or press enter 
+
+
+//runs formsubmithandler when someone click search or press enter 
 cityInputEl.addEventListener("submit", formSubmitHandler);
